0a094dde96484017944a0a2c24c9e44ee3274b2e
Goker Zafer <gokerlek@gmail.com>
2021-09-18 16:45:11 +0300

    table chart created

--- src/component/container.tsx
+++ src/component/container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Container: React.FC = ({ children }) => {
   return (
-    <div className=' h-screen flex font-serif text-sm justify-center items-center '>
+    <div className=' h-screen flex font-serif text-sm justify-center items-center  align-top'>
       {children}
     </div>
   );
--- src/component/table.tsx
+++ src/component/table.tsx
@@ -2,34 +2,42 @@ import React from "react";
 
 export const Table: React.FC = () => {
   return (
-    <div className='relative box-content w-400px h-400px border-2 border-DARK_GREY '>
-      <div className='absolute  transform translate translate-y-5 left-0 bottom-0 '>
-        <p>Completeness of Vision</p>
-      </div>
-      <div
-        style={{ bottom: "0", left: "0" }}
-        className='absolute  transform -translate-x-60 -translate-y-12  -rotate-90'>
-        Ability to Execute
-      </div>
-      <div className='absolute text-white bg-LIGHT_BLUE border-solid border-transparent p-1 my-2 rounded-md left-1/4 transform -translate-x-1/2'>
-        Challengers
-      </div>
-      <div className='absolute text-white bg-LIGHT_BLUE border-solid border-transparent p-1 my-2 rounded-md left-1/4 bottom-0px transform -translate-x-1/2'>
-        Niche Players
-      </div>
-      <div className='absolute text-white bg-LIGHT_BLUE border-solid border-transparent p-1 my-2 rounded-md right-1/4 transform translate-x-1/2'>
-        Leaders
-      </div>
-      <div className='absolute text-white bg-LIGHT_BLUE border-solid border-transparent p-1 my-2 rounded-md right-1/4 bottom-0px transform translate-x-1/2'>
-        Visionaries
-      </div>
-      <div className='absolute left-1/2 h-full w-2px bg-LIGHT_GREY transform -translate-x-1px'></div>
-      <div className='absolute top-1/2 w-full h-2px bg-LIGHT_GREY'></div>
-      <div
-        draggable='true'
-        style={{ top: "200px", left: "200px" }}
-        className='absolute rounded-full m-0 bg-DARK_BLUE w-4 h-4 transform origin-center -translate-x-1/2  -translate-y-1/2'
-      />
+    <div className='mx-2 h-400px '>
+      <button>Add</button>
+      <table className='table-fix border-separate'>
+        <thead className=''>
+          <tr>
+            <th className='w-64 border border-white rounded-lg text-white bg-LIGHT_BLUE text-sm font-normal font-serif'>
+              Label
+            </th>
+            <th className='w-16 border border-white rounded-lg text-white bg-LIGHT_BLUE text-sm font-normal font-serif'>
+              Vision
+            </th>
+            <th className='w-16 border border-white rounded-lg text-white bg-LIGHT_BLUE text-sm font-normal font-serif'>
+              Ability
+            </th>
+            <th className='w-28 border border-white rounded-lg text-white bg-LIGHT_BLUE text-sm font-normal font-serif'>
+              Delete
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className='border border-LIGHT_BLUE rounded-md  text-sm font-normal font-serif'>
+              <input type='text' />
+            </td>
+            <td className='border border-LIGHT_BLUE rounded-md   text-sm font-normal font-serif'>
+              <input type='number' />
+            </td>
+            <td className='border border-LIGHT_BLUE rounded-md text-sm font-normal font-serif'>
+              <input type='number' />
+            </td>
+            <td>
+              <button> delete </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
